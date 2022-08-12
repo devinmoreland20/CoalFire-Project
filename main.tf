3034fd6523e13940c3a669f5dbf6d168fa97222b
devinmoreland <devinmoreland20@gmail.com>
2022-08-12 17:26:30 -0400

    Almost complete

--- main.tf
+++ main.tf
@@ -66,33 +66,31 @@ module "loadbalancer" {
   listener_port          = 80
   listener_protocol      = "HTTP"
   lb_healthy_threshold   = 2
-  lb_unhealthy_threshold = 2
+  lb_unhealthy_threshold = 3
   lb_timeout             = 20
-  lb_interval            = 60
+  lb_interval            = 30
 
 }
 
 module "autoscaling" {
-  source                     = "./modules/autoscaling"
-  asg_name                   = join("-", ["webserver-asg", var.tags])
-  ami                        = "ami-092b43193629811af"
-  instance_type              = "t2.micro"
-  key_name                   = data.aws_key_pair.Ohio.key_name
-  private_sn                 = module.networking.private_subnet
-  launch_config_sg           = module.security.launch_config_sg
-  webserver_target_group_arn = module.loadbalancer.lb_tg_arn
-  max_size                   = 6
-  min_size                   = 2
-  desired_capacity           = 2
-  force_delete               = true
-  health_check_grace_period  = 450
-  health_check_type          = "ELB"
-  policy_type                = "TargetTrackingScaling"
-  predefined_metric_type     = "ASGAverageCPUUtilization"
-  adjustment_type            = "ChangeInCapacity"
-  target_value               = 50.0
-  #user_data                  = "${path.root}/userdata.tpl"
-
+  source                        = "./modules/autoscaling"
+  asg_name                      = join("-", ["webserver-asg", var.tags])
+  ami                           = "ami-051dfed8f67f095f5" #---the AMI in use in AL2.    ami-0618faebd88eb142c"  ---- this is the AMI with RHEL that I created. 
+  instance_type                 = "t2.micro"
+  key_name                      = data.aws_key_pair.Ohio.key_name
+  private_sn                    = module.networking.private_subnet
+  launch_config_sg              = module.security.launch_config_sg
+  webserver_target_group_arn    = module.loadbalancer.lb_tg_arn
+  max_size                      = 6
+  min_size                      = 2
+  desired_capacity              = 2
+  force_delete                  = true
+  health_check_grace_period     = 450
+  health_check_type             = "ELB"
+  policy_type                   = "TargetTrackingScaling"
+  predefined_metric_type        = "ASGAverageCPUUtilization"
+  adjustment_type               = "ChangeInCapacity"
+  target_value                  = 85
   root_block_device_encrypted   = true
   root_block_device_del_term    = true
   root_block_device_volume_type = "gp2"
@@ -120,12 +118,4 @@ module "WAF" {
   sampled_requests_enabled   = true
 
 }
-module "sns" {
-  source          = "./modules/sns"
-  asg_group_names = module.autoscaling.asg_name
-  notifications   = "autoscaling:EC2_INSTANCE_LAUNCH"
-  sns_topic_name  = "ASG-increased-capacity"
-  fifo_topic      = false
-  sub_protocol    = "email"
-  sub_endpoint    = "devinmoreland20@gmail.com"
-}
+
